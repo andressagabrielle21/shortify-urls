7b9a7aab60b15070d6ce7fd1fa730217d6d92835
andressagabrielle21 <andressagss21@gmail.com>
2022-03-26 11:09:29 -0300

    first commit

---
+++ shortner/static/js/script.js
@@ -0,0 +1,15 @@
+$(document).on('submit','#post-form',function(e){
+  e.preventDefault();
+
+  $.ajax({
+    type:'POST',
+    url:'/create',
+    data:{
+      link:$('#link').val(),
+      csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val(),
+    },
+    success: function(data){
+      $('h3').html("localhost:8000/"+data)
+    }
+  });
+});
